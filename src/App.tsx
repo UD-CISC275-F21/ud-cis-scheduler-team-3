dd351d2d6a94f615e7e3c182431c08e1879ba3aa
AHILYND <AHILYND@UDEL.EDU>
2021-10-12 20:00:18 -0400

    my name on site

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App(): JSX.Element {
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler
                 <p>
-                    Authors: Ren Ross, Abel Juarez, and 
+                    Authors: Ren Ross, Abel Juarez, and Ahilyn Dipre
                 </p>
             </header>
         </div>
