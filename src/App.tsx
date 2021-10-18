784aa2297a5f4f63f8e572eda2b936cb03da68ae
AHILYND <AHILYND@UDEL.EDU>
2021-10-17 23:45:27 -0400

    hopefully this copiles now

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,28 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <body>
-            <div className="App">
-
-                <h1>
+        <div className="App">
+        
+            <h1>
                 Welcome! 
-                    <p>This is UD CIS Scheduler Builder!
-                        <p>With this site you will be able to keep track of your credits to
+                <p>
+                With this site you will be able to keep track of your credits to
                             make sure that you graduate on time. You will be able to determine
                             how many credits you have, how many credits you need, etc.
-                        <p>
-                        Follow these steps:
-                        </p>
-                        </p>
-                    </p>
-                </h1>
-                <header>
+                    
+                                Follow these steps:
+                </p>
+            </h1>
+    
+            <header>
                 UD CIS Scheduler
 
-                    <p>
-                    Authors: Ren Ross, Abel Juarez, and Ahilyn Dipre
-
-                    </p>
-                </header>
+                <p>Authors: Ren Ross, Abel Juarez, and Ahilyn Dipre</p>
+            </header>
               
             
             
-            </div>
-        </body>
+        </div>
     );
 }
 
