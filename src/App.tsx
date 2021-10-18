e03c4d3c9287bc9f0785f9851018375c3e2e5c2d
AHILYND <AHILYND@UDEL.EDU>
2021-10-17 23:19:26 -0400

    blank page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
 import React from "react";
 import "./App.css";
-import Home from "./tabs/home";
-import TestDummy from "./tabs/testingdummy";
-import Navbar from "./components/Navbar";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"; 
 
-// Take three baby
-
-/*function App(): JSX.Element {
-    return (
-        <div className="App">
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route path='/home' exact component={Home} />
-                </Switch>
-            </Router>
-            <header className="App-header">
-                    UD CIS Scheduler
-                <p>
-                    Authors: Ren Ross, Abel Juarez, and Ahilyn Dipre
-                </p>
-            </header>
-            
-        </div>
-    );
-}
-*/
 function App(): JSX.Element {
     return (
         <div className="App">
-            <Router>
-                <header>
+           
+            <header>
                 UD CIS Scheduler
-                </header>
-                <Navbar />
-                <Switch>
-                    <Route path='/home' exact component={Home} />
-                    <Route path='/testingdummy' exact component={TestDummy} />
-                </Switch>
-            </Router>
+
+                <p>Authors: Ren Ross, Abel Juarez, and Ahilyn Dipre</p>
+            </header>
+              
             
             
         </div>
