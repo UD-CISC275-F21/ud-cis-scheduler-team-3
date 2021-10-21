21572f52ccd8e80888446b5d728ffd253347bed4
Abel Juarez <jabel@udel.edu>
2021-10-21 14:18:24 -0400

    Finally!!!!

--- src/Components/Courses.ts
+++ src/Components/Courses.ts
@@ -1,25 +1,27 @@
 export const Course = [{
-    "Course": "CISC 108",
+    "Code": "CISC 108",
     "Title": "Introdction to Computer Science I",
     "Credits": 3,
     "Description": "COREQ: MATH 115, MATH 117, or higher math course or math placement."
 },
 {
-    "Course": "EGGG 101",
+    "Code": "EGGG 101",
     "Title": "Introduction to Engineering (FYE)",
     "Credits": 2,
     "Description": "First Year Experience"
 },
 {
-    "Course": "MATH 241",
+    "Code": "MATH 241",
     "Title": "Analytic Geometry & Calculus A",
     "Credits": 4,
     "Description": " MATH 117 or acceptable score on the Math Placement Exam in accordance with current standards determined by the Department of Mathematical Sciences. RESTRICTIONS: Students who received credit in MATH 242 or MATH 243 are not eligible to take this course without permission."
 },
 {
-    "Course": "ENGL 110",
+    "Code": "ENGL 110",
     "Title": "Seminar in Composition",
     "Credits": 3,
     "Description": "Required of all students. Should be taken first year on campus"
 }
 ];
+
+export default Course;
--- src/Components/Table-Header.tsx
+++ src/Components/Table-Header.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 
-interface Course {
-    course: string;
+export interface Course {
+    code: string;
     title: string;
     credits: number;
     description: string;
 }
 
 export function CourseTable(): JSX.Element {
-        let courses: Course[] = [{course: "CISC 108", title: "Introdction to Computer Science I", credits: 3, description: "COREQ: MATH 115, MATH 117, or higher math course or math placement."},
-                                {course: "EGGG 101", title: "Introduction to Engineering (FYE)", credits: 2, description: "First Year Experience"},
-                                {course: "MATH 241", title: "Analytic Geometry & Calculus A", credits: 4, description: " MATH 117 or acceptable score on the Math Placement Exam in accordance with current standards determined by the Department of Mathematical Sciences. RESTRICTIONS: Students who received credit in MATH 242 or MATH 243 are not eligible to take this course without permission."},
-                                {course: "ENGL 110", title: "Seminar in Composition", credits: 3, description: "Required of all students. Should be taken first year on campus"}]
+    const courses: Course[] = [{code: "CISC 108", title: "Introdction to Computer Science I", credits: 3, description: "COREQ: MATH 115, MATH 117, or higher math course or math placement."},
+        {code: "EGGG 101", title: "Introduction to Engineering (FYE)", credits: 2, description: "First Year Experience"},
+        {code: "MATH 241", title: "Analytic Geometry & Calculus A", credits: 4, description: " MATH 117 or acceptable score on the Math Placement Exam in accordance with current standards determined by the Department of Mathematical Sciences. RESTRICTIONS: Students who received credit in MATH 242 or MATH 243 are not eligible to take this course without permission."},
+        {code: "ENGL 110", title: "Seminar in Composition", credits: 3, description: "Required of all students. Should be taken first year on campus"}];
     
-return (<table>
-    <tr><th>Course</th><th>Title</th><th>Credits</th><th>Description</th></tr>
-    { courses.map((course: Course) => {
-        return (<tr key={course.course}>
-            <td>{course.title}</td>
-            <td>{course.credits}</td>
-            <td>{course.description}</td>
-        </tr>)
-    })}
-</table>);
+    return (<table>
+        <tr><th>Course</th><th>Title</th><th>Credits</th><th>Description</th></tr>
+        { courses.map((course: Course) => {
+            return (<tr key={course.code}>
+                <td>{course.title}</td>
+                <td>{course.credits}</td>
+                <td>{course.description}</td>
+            </tr>);
+        })}
+    </table>);
 }
