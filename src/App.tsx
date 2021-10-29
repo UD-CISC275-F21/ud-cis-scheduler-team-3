4a6c89b918ed4c6e7b6fdd3f06a52065dd8ba59f
renross <briross@udel.edu>
2021-10-28 22:50:04 -0400

    added edit course button currently triggers a set of 3 prompts that are all logged to console

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,19 @@ import { SpringTable } from "./Components/Spring-Semester";
 import "./css/App.css";
 import Tabs from "./Components/tabs/Tabs";
 import Tab from "./Components/tabs/Tab";
+import "./css/editButton.css";
+import { editCourse } from "./editCourse";
 
-
-// import bootstrap
-
-
-function App (): JSX.Element {
-    //jsx.element is very important return type , function has to return jsx.element
+function App (): JSX.Element { //jsx.element is very important return type , function has to return jsx.element
     return (
-        <Tabs>
+        <><Tabs>
             <Tab title="Welcome and navigating our site">Welcome To Team 3s
                 <header>
-                UD CIS Scheduler
+                    UD CIS Scheduler
                     <p>Authors: Ren Ross, Abel Juarez, and Ahilyn Dipre</p>
                     <p>Our goal is to help CISC students plan out their semesters,
                         by providing templates of potential Fall/Spring semesters and even
-                        Winter and Summer. </p>
+                        Winter and Summer.</p>
                     <p>This site will serve as a way for students
                         to keep track of the classes they have taken and which ones
                         they need to take next.
@@ -33,13 +30,14 @@ function App (): JSX.Element {
                     <li>To be continued</li>
                 </ul>
             </Tab>
-            <Tab title ="Course selector">
+            <Tab title="Course selector">
+                <button className="editButton" onClick = { editCourse }>Edit Course</button>
                 <FallTable></FallTable>
-                
                 <SpringTable></SpringTable>
             </Tab>
-  
         </Tabs>
+          
+        </>
     );
 }
   
