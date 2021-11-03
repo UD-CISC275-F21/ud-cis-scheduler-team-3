3bc81df804a5458285580387b7e6fc94e421ce35
Abel Juarez <jabel@udel.edu>
2021-11-03 17:35:12 -0400

    Buttons work to a certain extent

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,18 @@ import { Semester, SemesterTable } from "./Components/SemesterTable";
 function App(): JSX.Element { // jsx.element = very important return type, function has to return jsx.element
     const [course, setCourse] = useState<CourseFF[]>(COURSES);
     const [visible, setVisible] = useState<boolean>(false);
-    
     const semesterList: Semester[] = [{
-        courses: [{code: "CISC 108", title: "Introdction to Computer Science I", credits: 3, description: "COREQ: MATH 115, MATH 117, or higher math course or math placement."},
-            {code: "EGGG 101", title: "Introduction to Engineering (FYE)", credits: 2, description: "First Year Experience"},
-            {code: "MATH 241", title: "Analytic Geometry & Calculus A", credits: 4, description: " MATH 117 or acceptable score on the Math Placement Exam in accordance with current standards determined by the Department of Mathematical Sciences. RESTRICTIONS: Students who received credit in MATH 242 or MATH 243 are not eligible to take this course without permission."},
-            {code: "ENGL 110", title: "Seminar in Composition", credits: 3, description: "Required of all students. Should be taken first year on campus"},
-            {code: "", title: "Breath Requirement Elective 1", credits: 3, description: "Required of all students."}],
+        courses: [COURSES[0],COURSES[1],COURSES[2],COURSES[3],COURSES[4]],
         title: "Freshman Fall Semester"
-    }, {    
-        courses: [{code: "CISC 181", title: "Introdction to Computer Science II", credits: 3, description: "PREREQ: Grade of C- or better in CISC108 or CISC106. COREQ: MATH221, MATH241, or a higher level math course or math placement."},
-            {code: "CISC 210", title: "Introduction to Systems Programming", credits: 3, description: "PREREQ: A grade of C- or better in CISC106 or CISC108. COREQ: MATH221 or MATH241 or a higher level math course or math placement."},
-            {code: "MATH 242", title: "Analytic Geometry & Calculus B", credits: 4, description: "PREREQ: MATH232 or MATH241. RESTRICTIONS: Students who received credit in MATH243 are not eligible to take this course without permission."},
-            {code: "", title: "Laboratory Science I", credits: 4, description: "Required of all students. Any science with a lab"},
-            {code: "", title: "Breath Requirement Elective 2", credits: 3, description: "Required of all students."}],
+    }, 
+    {    
+        courses: [COURSES[5],COURSES[6],COURSES[7],COURSES[8],COURSES[9]],
         title: "Freshman Spring Semester"
-    }, {
-        courses: [{code: "CISC 220", title: "Data Structures", credits: 3, description: "PREREQ: A minimum grade of C- in CISC210. COREQ: MATH210 or MATH241."},
-            {code: "CISC 260", title: "Machine Org. & Assembly Language", credits: 3, description: "PREREQ: A minimum grade of C- in CISC210."},
-            {code: "MATH 210", title: "Discrete Mathematics I", credits: 3, description: "COREQ: MATH241, MATH242 or MATH232"},
-            {code: "", title: "Laboratory Science 2", credits: 4, description: "Required of all students. Any science with a lab"},
-            {code: "", title: "Breath Requirement Elective 3", credits: 3, description: "Required of all students."}],
-        title: "Sophmore Fall Semester"}
+    },
+    {
+        courses: [COURSES[10],COURSES[11],COURSES[12],COURSES[13],COURSES[14]],
+        title: "Sophmore Fall Semester"
+    }
     ];
 
     const [semester, setSemester] = useState<Semester>(semesterList[0]);
@@ -71,7 +61,8 @@ function App(): JSX.Element { // jsx.element = very important return type, funct
                 <Tab title="Course selector">
                     <ControlPanel showEditModal={ setVisible }></ControlPanel>
                     <SemesterTable currentSemester={semester}></SemesterTable>
-                    <button onClick={() => setSemester(semesterList[0])}></button>
+                    <button onClick={() => setSemester(semesterList[0])}>Prev</button>
+                    <button onClick={() => setSemester(semesterList[1])}>Next</button>
                 </Tab>
             </Tabs>
             { /*<EditCourse visible={ visible } setVisible={ setVisible } editCourse={ EditCourse }></EditCourse> */}
