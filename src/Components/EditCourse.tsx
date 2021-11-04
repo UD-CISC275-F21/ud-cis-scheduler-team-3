d880ab20bbd54d546b762c2c28e001c47a140a85
Ren Ross <briross@udel.edu>
2021-11-04 00:15:55 -0400

    added args to EditCourse

--- src/Components/EditCourse.tsx
+++ src/Components/EditCourse.tsx
@@ -4,16 +4,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Course as CourseFF } from "./Fall-Semester";
 
 
+
 // https://github.com/UD-CISC275-F21/ta-trainer/blob/main/src/components/AddCardModal.tsx
-export function EditCourse({visible, setVisible}: {visible: boolean, setVisible: (b: boolean)=>void, editCourse: (c: CourseFF)=>void}): JSX.Element {
+export function EditCourse({visible, setVisible}: // , editCourse, semester, setSemester
+    {visible: boolean, setVisible: (b: boolean)=>void, editCourse: (c: CourseFF)=>void, semester: CourseFF[], setSemester: (s: CourseFF[])=>void}): JSX.Element {
     const [title, setTitle] = useState<string>("");
     const [description, setDescription] = useState<string>("");
     const hideModal = () => setVisible(false);
     // const courseEdit = prompt("Enter course code (ex: \"CISC 108\")");
-    
     /*
-    function editCourse(): void {
-        console.log("in editCourse()");
+    function mapCourses(): JSX.Element {
+        return (
+            <ul>
+                {semester.map((course) => 
+                    <li key={course.code}></li>
+                )}
+            </ul>
+        );
+    }
+    
+    function editableCourse({course}: {course: CourseFF}): JSX.Element {
+        return (
+            <div>
+                <div>{ course.code }</div>
+                <div>{ course.title }</div>
+                <div>{ course.credits }</div>
+                <div>{ course.description }</div>
+            </div>
+        );
+    }
+
+    function findCourse(): void {
+        console.log("woo!");
     }
     */
 
@@ -24,7 +46,7 @@ export function EditCourse({visible, setVisible}: {visible: boolean, setVisible:
     }
     
     return (
-        <Modal show={ visible } onHide={ hideModal }>
+        <Modal show={visible} onHide={hideModal}>
             <Modal.Header>
                 <Modal.Title>Edit Course</Modal.Title>
             </Modal.Header>
@@ -32,17 +54,17 @@ export function EditCourse({visible, setVisible}: {visible: boolean, setVisible:
                 <Form>
                     <Form.Group className='mb-3' id='editCourse.courseTitle'>
                         <Form.Label>Course Title</Form.Label>
-                        <Form.Control as="textarea" rows={1} value={ title } onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>) => setTitle(ev.target.value)}/>
+                        <Form.Control as="textarea" rows={1} value={title} onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>) => setTitle(ev.target.value)} />
                     </Form.Group>
                     <Form.Group className='mb-3' id='editCourse.courseDescription'>
                         <Form.Label>Course Description</Form.Label>
-                        <Form.Control as="textarea" rows={5} value={ description } onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>) => setDescription(ev.target.value)}/>
+                        <Form.Control as="textarea" rows={5} value={description} onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>) => setDescription(ev.target.value)} />
                     </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={ hideModal }>Cancel</Button>
-                <Button variant="primary" onClick={ saveEdit }>Save</Button>
+                <Button variant="secondary" onClick={hideModal}>Cancel</Button>
+                <Button variant="primary" onClick={saveEdit}>Save</Button>
             </Modal.Footer>
         </Modal>
     );
