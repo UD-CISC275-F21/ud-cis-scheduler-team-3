60dae6784b5008cf9fabeb5fed5056988707cf85
Ren Ross <briross@udel.edu>
2021-11-03 23:32:18 -0400

    edit course modal is functional

--- src/Components/EditCourse.tsx
+++ src/Components/EditCourse.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Course as CourseFF } from "./Fall-Semester";
 
 
