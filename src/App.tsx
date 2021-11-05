f1584d5ea4fcee93a1a3e272830fd2535e318e66
renross <briross@udel.edu>
2021-11-05 00:33:58 -0400

    edit forms display in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./css/App.css";
 import React, {useState} from "react";
-// import { Course as CourseIF } from "./Components/SemesterTable";
 import Tab from "./Components/Tab";
-import { EditCourse } from "./Components/EditCourse";
 import COURSES from "./Assets/Courses.json";
 import { Semester, SemesterTable } from "./Components/SemesterTable";
 
 function App(): JSX.Element { // jsx.element = very important return type, function has to return jsx.element
-    // const [semester, setSemester] = useState<CourseIF[]>(COURSES);
-    // const [visible, setVisible] = useState<boolean>(false);
     const semesterList: Semester[] = [{
         courses: [COURSES[0],COURSES[1],COURSES[2],COURSES[3],COURSES[4]],
         title: "Freshman Fall Semester"
@@ -44,11 +40,6 @@ function App(): JSX.Element { // jsx.element = very important return type, funct
     ];
 
     const [addSemester, settingSemester] = useState<Semester>(semesterList[0]);
-    /*
-    function editCourse(c: CourseIF): void {
-        setSemester([...semester, c]); // fix this line later
-    }
-    */
     return (
         <div>
             <Tab>
@@ -75,7 +66,7 @@ function App(): JSX.Element { // jsx.element = very important return type, funct
                     </ul>
                 </span>
                 <span title="Course selector">
-                    <SemesterTable currentSemester={addSemester}></SemesterTable>
+                    <SemesterTable currentSemester={ addSemester }></SemesterTable>
                     <a><button onClick={() => settingSemester(semesterList[0])}>1</button></a>
                     <a><button onClick={() => settingSemester(semesterList[1])}>2</button></a>
                     <a><button onClick={() => settingSemester(semesterList[2])}>3</button></a>
@@ -86,7 +77,6 @@ function App(): JSX.Element { // jsx.element = very important return type, funct
                     <a><button onClick={() => settingSemester(semesterList[7])}>8</button></a>
                 </span>
             </Tab>
-            <EditCourse></EditCourse>
         </div>
 
     );
--- src/Components/SemesterTable.tsx
+++ src/Components/SemesterTable.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { ToggleButton } from "react-bootstrap";
 import "../css/Semester.css";
-// import { ControlPanel } from "./ControlPanel";
+import { OpenEditing, CloseEditing } from "./EditorControlPanel";
+import { TitleForm, DescriptionForm } from "./EditorForms";
 
 export interface Course {
     code: string;
@@ -17,24 +17,41 @@ export interface Semester {
  
 export function SemesterTable({currentSemester}: {currentSemester: Semester}): JSX.Element {
     const [editing, setEditing] = useState<boolean>(false);
+    const [newTitle, setTitle] = useState<string>("Enter new title.");
+    const [newDescription, setDescription] = useState<string>("Enter new description.");
 
     return <table className  = "Table-Header">
         <tr><th>Course</th><th>Title</th><th>Credits</th><th>Description</th><th></th></tr>
         { currentSemester.courses.map((course: Course) => {
-            console.log("LOOK AT THESE:", course.code); 
-            return <tr key={course.code}>
-                <td>{course.code}</td>
-                <td>{course.title}</td>
-                <td>{course.credits}</td>
-                <td>{course.description}</td>
-                <td><ToggleButton 
-                    variant="secondary" 
-                    id={ `toggle-editing-${ course.code }` }
-                    checked={ editing } 
-                    value="1" 
-                    onChange={ (event)=>setEditing(event?.currentTarget.checked) } 
-                >Edit</ToggleButton></td>
-            </tr>;
+            return editing ? 
+                <tr key={course.code}>
+                    <td>{course.code}</td>
+                    <td><TitleForm
+                        newTitle={ newTitle } 
+                        setTitle={ setTitle } 
+                    ></TitleForm></td>
+                    <td>{course.credits}</td>
+                    <td><DescriptionForm
+                        newDescription={ newDescription } 
+                        setDescription={ setDescription }
+                    ></DescriptionForm></td>
+                    <td><CloseEditing 
+                        course={ course } 
+                        setEditing={ setEditing } 
+                        newTitle={ newTitle } 
+                        setTitle={ setTitle } 
+                        newDescription={ newDescription } 
+                        setDescription={ setDescription }
+                    ></CloseEditing></td>
+                </tr> 
+                : 
+                <tr key={course.code}>
+                    <td>{course.code}</td>
+                    <td>{course.title}</td>
+                    <td>{course.credits}</td>
+                    <td>{course.description}</td>
+                    <td><OpenEditing course={ course } editing={ editing } setEditing={ setEditing }></OpenEditing></td>
+                </tr>;
         })}
     </table>;
 }
