faaaafe8d008a16278755ee653eff3dfc3c83001
renross <briross@udel.edu>
2021-11-10 23:25:04 -0500

    users can now remove any course

--- src/Components/Modals&Forms.tsx
+++ src/Components/Modals&Forms.tsx
@@ -23,23 +23,23 @@ export function DescriptionForm({newDescription, setDescription}: {newDescriptio
     );
 }
 
-export function AddCourseModal({showModal, setShowModal, addCourse}: 
-    {showModal: boolean, setShowModal: (b:boolean)=>void, addCourse: (c: Course)=>void}): JSX.Element{
+export function AddCourseModal({showAddModal, setShowAddModal, addCourse}: 
+    {showAddModal: boolean, setShowAddModal: (b:boolean)=>void, addCourse: (c: Course)=>void}): JSX.Element{
     const [code, setCode] = useState<string>("EX: CISC 275");
     const [title, setTitle] = useState<string>("EX: Introduction to Software Engineering");
     const [credits, setCredits] = useState<string>("EX: 3");
     const [description, setDescription] = useState<string>("EX: Hi Dr. Bart");
-    const hideModal = () => setShowModal(false);
+    const hideAddModal = () => setShowAddModal(false);
 
     function saveCourse() {
         addCourse({
             code, title, credits, description
         });
-        hideModal();
+        hideAddModal();
     }
     
     return (
-        <Modal show={ showModal } onHide={ hideModal }>
+        <Modal show={ showAddModal } onHide={ hideAddModal }>
             <Modal.Header closeButton>
                 <Modal.Title>Add New Course</Modal.Title>
             </Modal.Header>
@@ -72,8 +72,40 @@ export function AddCourseModal({showModal, setShowModal, addCourse}:
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="outline-danger" onClick={ () => setShowModal(false) }>Close</Button>
+                <Button variant="outline-danger" onClick={ () => setShowAddModal(false) }>Cancel</Button>
                 <Button variant="info" onClick={ saveCourse }>Add</Button>
+            </Modal.Footer>
+        </Modal>);
+}
+
+export function RemoveCourseModal({showRemoveModal, setShowRemoveModal, removeCourse}:
+    {showRemoveModal: boolean, setShowRemoveModal: (b:boolean)=>void, removeCourse: (s:string)=>void}): JSX.Element {
+    const [removeCode, setRemoveCode] = useState<string>("EX: CISC 108");
+    const hideRemoveModal = () => setShowRemoveModal(false);
+    
+    function saveCode() {
+        removeCourse(removeCode);
+        hideRemoveModal();
+    }
+
+    return (
+        <Modal show={ showRemoveModal } onHide={ hideRemoveModal }>
+            <Modal.Header closeButton>
+                <Modal.Title>Add New Course</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="addCourseForm.promptTextArea">
+                        <Form.Label>Enter Course Code</Form.Label>
+                        <Form.Control as="textarea" rows={1}
+                            value={ removeCode }
+                            onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>) => setRemoveCode(ev.target.value)}/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={ () => setShowRemoveModal(false) }>Cancel</Button>
+                <Button variant="warning" onClick={ saveCode }>Remove</Button>
             </Modal.Footer>
         </Modal>);
 }
