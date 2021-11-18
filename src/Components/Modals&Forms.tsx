d9b5d981a40b48a074684c230f217aaec371aeed
renross <briross@udel.edu>
2021-11-18 17:31:09 -0500

    remove course is now a drop-down menu more user-friendly + comment from Dr. Bart on presentation

--- src/Components/Modals&Forms.tsx
+++ src/Components/Modals&Forms.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Course } from "../Interfaces/Course";
+import { Semester } from "../Interfaces/Semester";
 
 export function CodeForm({ course, newCode, setCode }: { course: Course, newCode: string, setCode: (newCode: string) => void }): JSX.Element {
     return (
@@ -106,8 +107,8 @@ export function AddCourseModal({ showAddModal, setShowAddModal, addCourse }:
         </Modal>);
 }
 
-export function RemoveCourseModal({ showRemoveModal, setShowRemoveModal, removeCourse }:
-    { showRemoveModal: boolean, setShowRemoveModal: (b: boolean) => void, removeCourse: (s: string) => void }): JSX.Element {
+export function RemoveCourseModal({ showRemoveModal, setShowRemoveModal, removeCourse, currentSemester }:
+    { showRemoveModal: boolean, setShowRemoveModal: (b: boolean) => void, removeCourse: (s: string) => void, currentSemester: Semester}): JSX.Element {
     const [removeCode, setRemoveCode] = useState<string>("");
     const hideRemoveModal = () => setShowRemoveModal(false);
 
@@ -117,23 +118,26 @@ export function RemoveCourseModal({ showRemoveModal, setShowRemoveModal, removeC
     }
 
     return (
-        <Modal show={showRemoveModal} onHide={hideRemoveModal}>
+        <Modal show={ showRemoveModal } onHide={ hideRemoveModal }>
             <Modal.Header closeButton>
                 <Modal.Title>Remove Course</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form>
                     <Form.Group className="mb-3" controlId="addCourseForm.promptTextArea">
-                        <Form.Label>Enter Course Code</Form.Label>
-                        <Form.Control as="textarea" placeholder={"CISC 108"} rows={1}
-                            value={removeCode} onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>) => setRemoveCode(ev.target.value)} />
-                        <Form.Text className="text-muted">Please enter the course&#39;s code here.</Form.Text>
+                        <Form.Control placeholder={"Select a course to remove."} readOnly />
+                        <Form.Select value={ removeCode } onChange={(ev: React.ChangeEvent<HTMLSelectElement>) => setRemoveCode(ev.target.value)}>
+                            {currentSemester.courses.map((course: Course): JSX.Element => {
+                                return <option key={ course.code }>{ course.code }</option>;
+                            })};
+                        </Form.Select>
+                        <Form.Text className="text-muted">Select the course you would like removed from this drop-down menu.</Form.Text>
                     </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={() => setShowRemoveModal(false)}>Cancel</Button>
-                <Button variant="warning" onClick={saveCode}>Remove</Button>
+                <Button variant="warning" onClick={ saveCode }>Remove</Button>
             </Modal.Footer>
         </Modal>);
 }
