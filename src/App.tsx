0d947c629810db9ca256239007f379c58d19e03e
renross <briross@udel.edu>
2021-11-19 15:56:17 -0500

    tab links fixed

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,14 @@ function App(): JSX.Element { // jsx.element = very important return type, funct
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand>Team 3 UD CIS Scheduler</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="/welcome">Welcome</Nav.Link>
-                <Nav.Link href="/course-scheduler">Course Scheduler</Nav.Link>
+                <Nav.Link href="/ud-cis-scheduler-team-3/welcome">Welcome</Nav.Link>
+                <Nav.Link href="/ud-cis-scheduler-team-3/course-scheduler">Course Scheduler</Nav.Link>
             </Nav>
         </Navbar>
         <Router>
             <Switch>
-                <Route path="/course-scheduler"><CourseScheduler/></Route>
-                <Route path="/welcome"><WelcomeMessage></WelcomeMessage></Route>
+                <Route path="/ud-cis-scheduler-team-3/course-scheduler"><CourseScheduler/></Route>
+                <Route path="/ud-cis-scheduler-team-3/welcome"><WelcomeMessage></WelcomeMessage></Route>
                 <Route path=""><WelcomeMessage></WelcomeMessage></Route>
             </Switch>
         </Router>
