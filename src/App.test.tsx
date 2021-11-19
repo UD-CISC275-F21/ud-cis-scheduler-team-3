260b7e61c8e78e44407242ecf7b648410810dcc4
renross <briross@udel.edu>
2021-11-19 15:35:55 -0500

    modified unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import PopUp from "./Components/PopUpInstructions";
-import TabTitle from "./Components/Tabs/TabTitle";
-
-
+// import PopUp from "./Components/PopUpInstructions";
+// import TabTitle from "./Components/Tabs/TabTitle";
 
 test("renders UD CIS Scheduler text", () => {
     render(<App />);
-    const linkElement = screen.getByText(/UD CIS Scheduler/i);
+    const linkElement = screen.getByText(/Welcome to Team 3's UD CIS Scheduler!/i);
     expect(linkElement).toBeInTheDocument();
 });
 
@@ -29,26 +27,32 @@ test("link for UD comp sci plan is provided", () => {
     render(<App />);
 });
 
+/* needs to be re-worked for new navbar
 test("Tabs are visible", () => {
     render(<App />);
 
     const Tab = screen.getByTestId;
     expect(Tab).toBeInTheDocument;
 });
+*/
 
 it("users can click button to get rid of courses", async () => {
     const removeCode = screen.getByTestId;
     expect(removeCode).toBeInTheDocument;
 });
 
+/* needs to be re-worked for toasts 
 test("all PopUpInstructions Props are getting used", () => {
     render(<PopUp content="Hello World" handleClose={() => null}/>);
 });
+*/
 
+/* needs to be re-worked for new navbar
 test("properties of TabTitle are being used", () => {
 
     render(<TabTitle title="Name of tab" index={0} setSelectedTab={() => null} selectedTab={0}/>);
 });
+*/
 
 test("hardReset is visible", () => {
     render(<App />);
