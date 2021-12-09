6f3a0abb16ab3a083510fd9a517fc2fcae027bf1
renross <briross@udel.edu>
2021-12-09 01:53:13 -0500

    updated config values

--- src/Assets/config-example.ts
+++ src/Assets/config-example.ts
@@ -1,14 +1,1 @@
-const REPO_OWNER = "";
-const REPO_PROJECT = "";
-const USER_ISSUES_TOKEN = "";
-const USER_ISSUES_AGENT = "";
-
-export const REPO_AUTH = {
-    owner: REPO_OWNER, 
-    project: REPO_PROJECT
-};
-
-export const OCTOKIT_AUTH = {
-    auth: USER_ISSUES_TOKEN,
-    userAgent: USER_ISSUES_AGENT
-};
+export const USER_ISSUES_TOKEN = "";
